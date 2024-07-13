31a71d46bb77d0a2de94c29d469a5391c9f86592
charkster <31560568+charkster@users.noreply.github.com>
2024-07-13 12:44:10 -0700

    Update AP33772.py

--- AP33772.py
+++ AP33772.py
@@ -9,7 +9,7 @@ class AP37772:
     def write_data(self, address=0x00, data=0x00): # write data
         num_bytes = (data.bit_length() + 7) // 8
 #        self.i2c.writeto_mem(self.slave_id, address, data.to_bytes(num_bytes, "little")) # MicroPython
-    self.i2c.write_i2c_block_data(self.slave_id, address, list(data.to_bytes(num_bytes,"little"))) # Raspberry Pi
+        self.i2c.write_i2c_block_data(self.slave_id, address, list(data.to_bytes(num_bytes,"little"))) # Raspberry Pi
     
     def read_data(self, address=0x00, num_bytes=1):
 #        int_read_byte = int.from_bytes(self.i2c.readfrom_mem(self.slave_id, address, num_bytes), "little") # MicroPython
